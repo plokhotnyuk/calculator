839024d6189486c778df26cd20137a1bbb9f6568
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2012-07-14 11:48:11 +0300

    added maven repo with installed 3rd-party dependencies

--- repo/install.bat
+++
@@ -1,3 +0,0 @@
-start mvn install:install-file -Dfile=windowlicker-core-dev.jar -DgroupId=com.googlecode.windowlicker -DartifactId=windowlicker-core -Dversion=268 -Dsources=windowlicker-core-dev-src.jar -Dpackaging=jar
-start mvn install:install-file -Dfile=windowlicker-swing-dev.jar -DgroupId=com.googlecode.windowlicker -DartifactId=windowlicker-swing -Dversion=268 -Dsources=windowlicker-swing-dev-src.jar -Dpackaging=jar
-start mvn install:install-file -Dfile=yatspec-172.jar -DgroupId=com.googlecode.yatspec -DartifactId=yatspec -Dversion=172 -Dsources=yatspec-172-sources.jar -Dpackaging=jar
